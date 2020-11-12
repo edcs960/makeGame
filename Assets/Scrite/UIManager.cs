0a4ef189312dcb109c6647fb27bb183381e9e72b
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-13 00:18:35 +0900

    게임 프로토타입 과제

---
+++ Assets/Scrite/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ButtonClick(string type)
+    {
+        switch (type)
+        {
+            case "START" :
+                SceneManager.LoadScene("Main");
+                break;
+            case "SETTING" :
+                // 팝업창으로 설정창 만들기
+                break;
+        }
+    }
+}
