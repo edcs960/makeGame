e15235ec4f4b061fb840ca296f675d36a26d68cf
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-13 22:27:45 +0900

    플레이어 관련 스크립트
    플레이어 애니메이션, 몬스터에게 데이지 입히는 함수 구현
    추후 일정구간을 반복적으로 왔다갔다하는 플레이어 이동 구현, 몬스터와 마주칠경우 공격하는 함수 구현

---
+++ PlayerCS.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCS : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject Monster;
+    public int dmg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Monster == null)
+        {
+            PlayerAnim();
+        }
+        else if(Monster != null)
+        {
+            PlayerAttackAnim();
+        }
+    }
+
+    public void SetDamage()
+    {
+        Monster.GetComponent<ZombiCS>().GetDamage(dmg); // 플레이어가 몬스터에게 공격하여 데미지를 입히는 코드
+    }
+
+    void PlayerAnim()
+    {
+        anim.SetInteger("AnimState",0);
+    }
+
+    void PlayerAttackAnim()
+    {
+        anim.SetInteger("AnimState",1);
+    }
+
+    void PlayerMove()
+    {
+        
+    }
+    
+}
