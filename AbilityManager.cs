2979b28ac5539f8b7cfc866340e3d8aef7458f8c
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-13 22:30:09 +0900

    플레이어 능력치 스크립트
    힘, 민첩, 지능, 행운을 이용하여 해당 능력치에 대한 능력 향상을 구현할 예정

---
+++ AbilityManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityManager : MonoBehaviour
+{
+    public static int money; // 재화
+
+    public static int Str; // 공격력?
+    public static int Dex; // 공격속도
+    public static int Int; // 크리티컬 확률
+    public static int Luk; // 흭득 재화 2배
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = 1000;
+        Str = 0;
+        Dex = 0;
+        Int = 0;
+        Luk = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text[] mainCosts = GameObject.FindGameObjectWithTag("UI").GetComponentsInChildren<Text>();
+
+        mainCosts[2].text = money.ToString();
+    }
+}
