b98c9ea854b287163ccd6eef977c769e2b83264d
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-16 23:30:16 +0900

    플레이어 스크립트
    플레이어가 이동하는 도중 maxX값과 minX값 이상으로 넘어가서 버그가 생기는 부분 해결

--- PlayerCS.cs
+++ PlayerCS.cs
@@ -50,34 +50,35 @@ public class PlayerCS : MonoBehaviour
 
     void PlayerWalkAnim()
     {
-        anim.SetInteger("AnimState",0);
+        anim.SetInteger("AnimState",0); // 이동 애니메이션
     }
 
     void PlayerAttackAnim()
     {
-        anim.SetInteger("AnimState",1);
+        anim.SetInteger("AnimState",1); // 공격 애니메이션
     }
 
     void PlayerMove()
     {
         if(Time.time >= StartTime)
         {
-            transform.position += new Vector3(moveSpeed * Time.deltaTime * sign,0,0);
+            transform.position += new Vector3(moveSpeed * Time.deltaTime * sign,0,0); // 플레이어 이동 코드
 
-            if(transform.position.x < minX + 0.3f || transform.position.x > maxX - 0.3f)
+            if(transform.position.x < minX || transform.position.x > maxX)
             {
-                sign *= -1;
-                PlayerScale_x *= -1;
-                transform.localScale = new Vector3(PlayerScale_x,0.7797894f,1);
+                transform.position += new Vector3(moveSpeed * Time.deltaTime * sign - 0.1f * sign,0,0); // minX와 maxX 이상 넘어갈 때
+                sign *= -1; // 방향 전환
+                PlayerScale_x *= -1; 
+                transform.localScale = new Vector3(PlayerScale_x,0.7797894f,1); // 플레이어가 보는 방향 전환
             }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other) // 여기는 쟤랑 만나면 자동으로 들어가져
+    void OnTriggerEnter2D(Collider2D other) // 여기는 몬스터랑 만나면 자동으로 들어가져
     {
         if(other.gameObject.tag == "Monster")
         {
-            coll = 1;
+            coll = 1; // 몬스터와 만났을때
         }
     }
 }
