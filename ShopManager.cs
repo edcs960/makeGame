9377055c6c935bd1ccb0c4899d8dda1ca68b8e18
edcs960 <65010544+edcs960@users.noreply.github.com>
2020-11-20 22:20:54 +0900

    상점 오픈하는 스트립트
    현재 버튼이 클릭되지 않는 현상 발견.... 조만간 고칠 예정

---
+++ ShopManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    GameObject Ability;
+    GameObject Weapon;
+
+    private int shop;
+    public void openshop(string shopname)
+    {
+        switch (shopname)
+        {
+            case "Ability" :
+                Ability = GameObject.FindGameObjectWithTag("Abilityshop");
+                shop = 1;
+                Ability.SetActive(true);
+                break;
+            case "Weapon" :
+                Weapon = GameObject.FindGameObjectWithTag("Weaponshop");
+                shop = 2;
+                Weapon.SetActive(true);
+                break;        
+            case "exit" :
+                if(shop == 1)
+                    Ability.SetActive(false);
+                else if(shop == 2)
+                    Weapon.SetActive(false);
+                shop = 0;
+                break;
+        }
+    }
+}
